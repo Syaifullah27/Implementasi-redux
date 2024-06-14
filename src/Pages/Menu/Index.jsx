e129959a4b589ed4de9ffa501ede42beead81a47
Esyeha <syh48149@gmail.com>
2024-06-14 15:47:55 +0700

    add responsive for menu page

--- src/Pages/Menu/Index.jsx
+++ src/Pages/Menu/Index.jsx
@@ -15,36 +15,49 @@ const MenuPage = () => {
 
 
     const handleLogout = () => {
-        localStorage.removeItem("access_token")
-        window.location.href = "/login"
+        const logout = confirm("Are you sure you want to log out?")
+        if(logout){
+            localStorage.removeItem("access_token")
+            window.location.href = "/login"
+        }
     }
 
     
     return (
-        <div className="h-screen bg-[#333333]">
+        <div className="h-full bg-[#333333] pb-10">
             <Navbar userName={decoded.userData.username} handleLogout={handleLogout}/>
 
-            <div className="w-11/12 mx-auto">
-                <Link to="/" >
-                    <button className=" py-2 text-xl font-medium underline border-none text-[#f5f5f5] hover:text-[#00ff95] transition-all duration-500 ease-in-out"
-                    > Back To Home</button>
-                </Link>
-            </div>
-
-            <div className="w-9/12 mx-auto">
-                <div className="mt-4 flex gap-2 justify-center items-center ">
-                    <span className="w-9/12 bg-gray-200 h-[1px]"></span>
-                    <h1 className='text-center text-3xl w-96 font-medium text-[#f5f5f5]'>Menu Page</h1>
-                    <span className="w-9/12 bg-gray-200 h-[1px]"></span>
+            <div className="pt-28 max-[765px]:pt-20">
+                <div className="w-11/12 mx-auto">
+                    <Link to="/" >
+                        <button className=" py-2 text-xl font-medium underline border-none text-[#f5f5f5] hover:text-[#00ff95] transition-all duration-500 ease-in-out max-sm:text-sm"
+                        > Back To Home</button>
+                    </Link>
                 </div>
-            </div>
 
-
-            <div className="w-11/12 mx-auto">
-                <div className="bg-[#f5f5f5] p-4 w-[210px] rounded-xl shadow-slate-950] shadow-2xl">
-                    <img src="kaiden_2.jpeg" alt="" width={200} className=" h-72 rounded-xl"/>
-                    <h2 className="text-center font-medium text-xl pt-2">Eleceed</h2>
+                <div className="w-9/12 mx-auto">
+                    <div className="mt-4 flex gap-2 justify-center items-center ">
+                        <span className="w-9/12 bg-gray-200 h-[1px]"></span>
+                        <h1 className='text-center text-3xl w-96 font-medium text-[#f5f5f5] max-sm:text-xl'>Menu Page</h1>
+                        <span className="w-9/12 bg-gray-200 h-[1px]"></span>
+                    </div>
                 </div>
+
+
+                <div className="flex w-11/12 mx-auto gap-10 mt-10 flex-wrap">
+                        <div className="bg-[#f5f5f5] p-4 w-[210px] rounded-xl shadow-slate-950] shadow-2xl max-sm:w-full">
+                            <img src="kaiden_2.jpeg" alt="" width={200} className=" h-72 rounded-xl max-sm:w-full"/>
+                            <p className="text-center font-medium text-xl pt-2">Kaiden</p>
+                            <h2 className="text-center font-medium text-sm">Eleceed</h2>
+                        </div>
+
+
+                        <div className="bg-[#f5f5f5] p-4 w-[210px] rounded-xl shadow-slate-950] shadow-2xl max-sm:w-full">
+                            <img src="lloyd.jpeg" alt="" width={200} className=" h-72 rounded-xl max-sm:w-full"/>
+                            <p className="text-center font-medium text-xl pt-2">Lloyd</p>
+                            <h2 className="text-center font-medium text-sm">{"The Greatest Estate Developer"}</h2>
+                        </div>
+                    </div>
             </div>
 
 
--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -1,27 +1,69 @@
+import { useState } from "react"
 import "./style.scss"
 
 // eslint-disable-next-line react/prop-types
 const Navbar = ({userName, handleLogout}) => {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
 
     return (
-        <div className="bg-[#ffcb10] shadow-xl">
-            <div className="flex justify-between w-11/12 mx-auto py-2">
-                <img src="./public/EsyehaX-logo.png" alt="logo" width={230} className="" />
-                <div className="flex gap-4 justify-center items-center">
-                    <div className="flex gap-4 justify-center items-center mr-4">
+        // <div className="bg-[#ffcb10] shadow-xl">
+        //     <div className="flex justify-between w-11/12 mx-auto py-2">
+        //         <img src="./public/EsyehaX-logo.png" alt="logo" width={230} className="max-sm:w-44" />
+        //         <div className="flex gap-4 justify-center items-center  ">
+        //             <button 
+        //             onClick={handleToggle}
+        //             className="font-medium md-[600px]:hidden rotate-90 text-2xl">|||</button>
+        //             <div className="flex gap-4 justify-center items-center mr-4 max-sm:hidden">
+        //                 <h3 className="font-medium  text-2xl text-[#333333]">{userName ? userName : ""}</h3>
+        //                 <img src="./public/kaiden_1.jpeg" alt="" width={50} height={50} className="rounded-full " />
+        //             </div>
+        //             <div className="button-container-1 max-sm:hidden">
+        //                 <span className="mas font-medium">{userName ? "Log Out" : "Login"}</span>
+        //                 <button onClick={handleLogout}
+        //                 id='work' type="button" name="Hover" className="font-medium">{userName ? "Log Out" : "Login"}</button>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div style={{
+            boxShadow: "0px 0px 16px rgba(0, 0, 0, 0.1)"}}
+            className="navbar-wrapper">
+                <nav className="navbar">
+                    <img src="./public/EsyehaX-logo.png"  width={230} className="max-sm:w-44"/>
+                    <div className="menu-toggle" onClick={toggleMenu}>
+                        <input type="checkbox" />
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <ul className={`menu-list  ${isMenuOpen ? 'active' : ''}`}>
+                        <div className="username">
+                            <h1 className="title-username">{userName}</h1>
+                            <img src="./public/kaiden_1.jpeg" alt="" width={50} height={50} className="rounded-full " />
+                        </div>
+                        <div className="button-container-1">
+                            <span className="mas font-medium">{userName ? "Log Out" : "Login"}</span>
+                            <button onClick={handleLogout}
+                            id='work' type="button" name="Hover" className="font-medium">{userName ? "Log Out" : "Login"}</button>
+                        </div>
+                    {/* <div className="flex gap-4 justify-center items-center mr-4 max-sm:hidden">
                         <h3 className="font-medium  text-2xl text-[#333333]">{userName ? userName : ""}</h3>
                         <img src="./public/kaiden_1.jpeg" alt="" width={50} height={50} className="rounded-full " />
                     </div>
-                    <div className="button-container-1">
+                    <div className="button-container-1 max-sm:hidden ">
                         <span className="mas font-medium">{userName ? "Log Out" : "Login"}</span>
                         <button onClick={handleLogout}
                         id='work' type="button" name="Hover" className="font-medium">{userName ? "Log Out" : "Login"}</button>
-                    </div>
-                </div>
+                    </div> */}
+                    </ul>
+                </nav>
             </div>
-        </div>
     )
 }
 
