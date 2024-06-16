9a108e2788d6c9ba88340d5889f40744517768e3
Esyeha <syh48149@gmail.com>
2024-06-16 12:32:38 +0700

    add add input menu

--- src/Pages/Menu/Index.jsx
+++ src/Pages/Menu/Index.jsx
@@ -17,7 +17,7 @@ const MenuPage = () => {
             .then((res) => {
                 const data = res?.data?.data?.Data
                 setDatasMenu(data)
-                // console.log(data);
+                console.log(data);
             })
             .catch((err) => console.log(err))
     }
@@ -68,19 +68,49 @@ const MenuPage = () => {
     }
 
 
-    const alertDelete = () => {
-        return (
-        <>
-            <div className="fixed top-1/4 left-1/2 ml-[-90px] border-2 border-[#f5f5f5] px-4 py-2 rounded-md bg-red-500 text-[#f5f5f5] pop-up-delete">
-                <h1>Berhasil diHapus</h1>
-            </div>
-        </>
-        )
+    // const alertDelete = () => {
+    //     return (
+    //     <>
+    //         <div className="fixed top-1/4 left-1/2 ml-[-90px] border-2 border-[#f5f5f5] px-4 py-2 rounded-md bg-red-500 text-[#f5f5f5] pop-up-delete">
+    //             <h1>Berhasil diHapus</h1>
+    //         </div>
+    //     </>
+    //     )
+    // }
+
+    // useEffect(() => {
+    //     alertDelete()
+    // }, [getMenu])
+
+    const [name, setName] = useState("")
+    const [type, setType] = useState("")
+    const [desc, setDesc] = useState("")
+    const [price, setPrice] = useState()
+
+
+    const handleName = (e) => {
+        e.preventDefault()
+        setName(e.target.value)
     }
 
-    useEffect(() => {
-        alertDelete()
-    }, [getMenu])
+    const handleType = (e) => {
+        e.preventDefault()
+        setType(e.target.value)
+    }
+
+    const handleDesc = (e) => {
+        e.preventDefault()
+        setDesc(e.target.value)
+    }
+
+    const handlePrice = (e) => {
+        e.preventDefault()
+        setPrice(e.target.value)
+    }
+
+
+
+
 
 
     return (
@@ -95,8 +125,52 @@ const MenuPage = () => {
                         > Back To Home</button>
                     </Link>
                 </div>
-                
-{alertDelete}
+
+
+
+                <div className="w-11/12 mx-auto pb-10">
+                        <h1 className=" text-2xl pb-2 font-medium text-[#f5f5f5] text-center">Add Your Menu</h1>
+                    <div className="flex justify-center items-center gap-5 pb-5 border-2 border-[#f5f5f5] px-4 py-2 rounded-md  text-[#f5f5f5]">
+                        <div className="flex gap-2 flex-col ">
+                            <label className="text-[#f5f5f5] font-medium">Name</label>
+                            <input 
+                                value={name}
+                                onChange={handleName}
+                                className={` w-56 h-10 bg-gray-200 text-[#333333] py-2 font-medium rounded-xl placeholder:text-sm  pl-2 placeholder:text-gray-400 outline-none ${name ? "hide-arrow" : ""}`}
+                                placeholder="add your favorite menu " />
+                        </div>
+                        <div className="flex gap-2 flex-col ">
+                            <label className="text-[#f5f5f5] font-medium">Description</label>
+                            <textarea 
+                                value={desc}
+                                onChange={handleDesc}
+                                className={`pt-2 w-56 h-10 bg-gray-200 text-[#333333] py-2 font-medium rounded-xl placeholder:text-sm  pl-2 placeholder:text-gray-400 outline-none ${desc ? "hide-arrow" : ""}`}
+                                placeholder="Describe your favorite menu " />
+                        </div>
+                        <div className="flex gap-2 flex-col ">
+                            <label className="text-[#f5f5f5] font-medium">Type</label>
+                            <select
+                                value={type}
+                                onChange={handleType}
+                                defaultValue={""}
+                                className={`w-56 h-10 bg-gray-200 text-gray-400 py-2 font-medium rounded-xl placeholder:text-sm  pl-2 placeholder:text-gray-300" ${type ? "hide-arrow" : ""}`}>
+                                <option value="" disabled hidden>Type</option>
+                                <option value="beverage" className="text-[#333333]">beverage</option>
+                            </select>
+                        </div>
+                        <div className="flex gap-2 flex-col ">
+                            <label className="text-[#f5f5f5] font-medium">Price</label>
+                            <input 
+                                value={price}
+                                onChange={handlePrice}
+                                type="number" placeholder="Price of your favorite menu" 
+                                className={`w-56 h-10 bg-gray-200 text-[#333333] py-2 font-medium rounded-xl placeholder:text-sm  pl-2 placeholder:text-gray-400 outline-none ${price ? "hide-arrow" : ""}`}/>
+                        </div>
+
+                        <button className="w-24 h-10 mt-7 rounded-xl bg-[#f5f5f5] text-[#333333] py-2 font-medium hover:bg-[#00ff95] hover:text-[#f5f5f5] transition-all duration-500 ease-in-out">Add</button>
+                    </div>
+                </div>
+
 
                 <div className="w-9/12 mx-auto">
                     <div className="mt-4 flex gap-2 justify-center items-center ">
@@ -105,6 +179,7 @@ const MenuPage = () => {
                         <span className="w-9/12 bg-gray-200 h-[1px]"></span>
                     </div>
                 </div>
+
 
 
                 <div className="flex w-11/12 mx-auto gap-10 mt-10 justify-center flex-wrap">
